28dbc06b8434f4342b9a087aa95aeb1df5b71062
Veve <everton7ej@gmail.com>
2022-10-04 19:55:44 -0300

    trying to deploy

--- src/main/java/br/com/squirtle/controller/ApiIndexController.java
+++ src/main/java/br/com/squirtle/controller/ApiIndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/api/v1/", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value="/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ApiIndexController {
 
     @GetMapping
