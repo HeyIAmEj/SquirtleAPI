92c1b96dd4b487000af76beca6cd03759ab8846d
Veve <everton7ej@gmail.com>
2022-10-04 21:02:30 -0300

    trying to deploy

--- src/main/java/br/com/squirtle/SquirtleApplication.java
+++ src/main/java/br/com/squirtle/SquirtleApplication.java
@@ -3,13 +3,14 @@ package br.com.squirtle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SquirtleApplication {
 
 	public static void main(String[] args) {
