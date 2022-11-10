3aa1ba6fc9ba1c2f8cc96c286978d9a9691ffbab
Veve <everton7ej@gmail.com>
2022-11-10 20:57:05 -0300

    updating the controller handler to the sensors data

--- src/main/java/br/com/squirtle/controller/SensorController.java
+++ src/main/java/br/com/squirtle/controller/SensorController.java
@@ -33,12 +33,29 @@ public class SensorController {
     @PutMapping("/{device_id}/sensores")
     public HashMap<String, String> alterarSensoralterarSensores(@PathVariable Long device_id, @RequestBody DispositivoDTO dispositivoDTO) throws DispositivoNaoEncontradoException {
         DispositivoDTO deviceBD = dispositivoService.getDeviceById(device_id);
+
+        String[] sensor_full = deviceBD.getSensor1().split(",");
+        String s1_value = String.join(",", sensor_full[0], sensor_full[1], sensor_full[2], String.join(":", sensor_full[3].split(":")[0], dispositivoDTO.getSensor1()));
+
+        sensor_full = deviceBD.getSensor2().split(",");
+        String s2_value = String.join(",", sensor_full[0], sensor_full[1], sensor_full[2], String.join(":", sensor_full[3].split(":")[0], dispositivoDTO.getSensor2()));
+
+        sensor_full = deviceBD.getSensor3().split(",");
+        String s3_value = String.join(",", sensor_full[0], sensor_full[1], sensor_full[2], String.join(":", sensor_full[3].split(":")[0], dispositivoDTO.getSensor3()));
+
+        sensor_full = deviceBD.getSensor4().split(",");
+        String s4_value = String.join(",", sensor_full[0], sensor_full[1], sensor_full[2], String.join(":", sensor_full[3].split(":")[0], dispositivoDTO.getSensor4()));
+
+        sensor_full = deviceBD.getSensor5().split(",");
+        String s5_value = String.join(",", sensor_full[0], sensor_full[1], sensor_full[2], String.join(":", sensor_full[3].split(":")[0], dispositivoDTO.getSensor5()));
+
+
         deviceBD.setStatus(dispositivoDTO.getStatus());
-        deviceBD.setSensor1(dispositivoDTO.getSensor1());
-        deviceBD.setSensor2(dispositivoDTO.getSensor2());
-        deviceBD.setSensor3(dispositivoDTO.getSensor3());
-        deviceBD.setSensor4(dispositivoDTO.getSensor4());
-        deviceBD.setSensor5(dispositivoDTO.getSensor5());
+        deviceBD.setSensor1(s1_value);
+        deviceBD.setSensor2(s2_value);
+        deviceBD.setSensor3(s3_value);
+        deviceBD.setSensor4(s4_value);
+        deviceBD.setSensor5(s5_value);
 
         Dispositivo deviceUpdated = dispositivoMapper.toModel(deviceBD);
         dispositivoService.updateSensor(deviceUpdated);
